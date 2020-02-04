b778bbf272f02039effad770eb3f1cf69885fd82
Timofei Tverdokhlebov <great.timofey@icloud.com>
2020-02-04 12:16:02 +0700

    fix height -> deviceHeight

--- src/components/Player/styles.js
+++ src/components/Player/styles.js
@@ -1,23 +1,23 @@
 import { StatusBar, StyleSheet } from 'react-native'
 
-import { height } from '@global/styles'
+import { deviceHeight } from '@global/styles'
 
 export default StyleSheet.create({
   common: {
-    position: 'absolute'
+    position: 'absolute',
   },
   first: {
-    top: height * 0.4 + StatusBar.currentHeight,
+    top: deviceHeight * 0.4 + StatusBar.currentHeight,
     left: 0,
-    transform: [{ rotate: '270deg' }]
+    transform: [{ rotate: '270deg' }],
   },
   second: {
     alignSelf: 'center',
-    transform: [{ rotate: '180deg' }]
+    transform: [{ rotate: '180deg' }],
   },
   third: {
-    top: height * 0.4 + StatusBar.currentHeight,
+    top: deviceHeight * 0.4 + StatusBar.currentHeight,
     right: 0,
-    transform: [{ rotate: '90deg' }]
-  }
+    transform: [{ rotate: '90deg' }],
+  },
 })
