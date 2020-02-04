52729acf7dbc98cdb65f94360fcdd94f02cf1b19
Timofei Tverdokhlebov <great.timofey@icloud.com>
2020-02-04 14:52:21 +0700

    add splash screen android

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { StatusBar, YellowBox } from 'react-native'
+import React, { useEffect } from 'react'
+import { StatusBar } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 import { Provider } from 'react-redux'
 import { Buffer } from 'buffer'
+import RNBootSplash from 'react-native-bootsplash'
 
 import { configureStore } from './src/redux'
 import { AppNavigator } from './src/navigation'
@@ -16,6 +17,10 @@ console.disableYellowBox = true
 const { store, persistor } = configureStore()
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({ duration: 250 })
+  }, [])
+
   return (
     <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#000000" />
